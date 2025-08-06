042565be4b842938421575320f8325f2c30f8bcd
Monir Hossain <monirhrabby.personal@gmail.com>
2025-08-06 15:48:32 +0600

    added: treewrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import Hero from "@/components/Home/Hero";
 import ProductCard from "@/components/shared/cards/product-card";
+import TreeWrapper from "@/providers/tree-wrapper";
 
 const Page = () => {
   return (
     <div>
-      <div
-       
-      >
+      <div>
         <Hero />
       </div>
 
-      <div className="bg-white py-[100px] container w-full h-auto grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-[24px] mt-[200px]">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+      <TreeWrapper>
+        <div className="bg-white py-[100px] container w-full h-auto grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-[24px] mt-[200px]">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </TreeWrapper>
     </div>
   );
 };

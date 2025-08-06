e8e55d77fc0551f20891a4f18e9d4157a30a37d3
Monir Hossain <monirhrabby.personal@gmail.com>
2025-08-06 17:11:13 +0600

    developed: product search

--- src/components/shared/cards/product-card.tsx
+++ src/components/shared/cards/product-card.tsx
@@ -1,40 +1,58 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { truncate } from "@/lib/utils";
+import { Product } from "@/types/products";
 import Image from "next/image";
+import Link from "next/link";
 
-const ProductCard = () => {
+interface ProductCardProps {
+  data?: Product;
+}
+
+const ProductCard = ({ data }: ProductCardProps) => {
   return (
     <Card className="w-full max-w-sm rounded-[14px] overflow-hidden shadow-lg">
       <div className="relative h-[236px]">
         <Image
-          src="https://images.pexels.com/photos/2731667/pexels-photo-2731667.jpeg"
+          src={
+            data?.photo[0] ??
+            "https://images.pexels.com/photos/2731667/pexels-photo-2731667.jpeg"
+          }
           fill
-          alt="Cannabis Product"
+          alt={data?.name || "Product Image"}
           className="object-cover"
         />
       </div>
       <CardContent className="p-4 space-y-3">
-        <div className="flex items-center gap-2">Cannabis Product 1</div>
+        <div className="flex items-center gap-2">{data?.name}</div>
 
         <div className="flex items-center gap-1 p-[8px] bg-[#F0EDF9] justify-between">
-          <span className="font-semibold">Relaxing</span>
+          <span className="font-semibold">
+            {data?.experiences.map((exp) => exp).join(", ")}
+          </span>
           <div>
             <div className="font-semibold flex items-center gap-1">
               <Image src="/icons/high.svg" alt="High" width={15} height={15} />{" "}
-              High
+              {data?.dosage}
             </div>
           </div>
         </div>
 
         <p className="text-sm text-gray-600 leading-relaxed">
-          Tom Yum Soup is a classic aromatic Thai dish known for its bold
-          combination of spicy, sour, salty, and sweet flavors.
+          {truncate(data?.description ?? "", 126)}
         </p>
 
         <div className="flex items-center justify-between pt-2">
-          <span className="text-2xl font-bold text-gray-900">$20.45</span>
-          <Button className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg font-medium">
-            Buy Now
+          <span className="text-2xl font-bold text-gray-900">
+            ${data?.prices[0].price}
+          </span>
+          <Button
+            className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg font-medium"
+            asChild
+          >
+            <Link href={`/products/${data?.name}`} className="w-fit">
+              Buy Now
+            </Link>
           </Button>
         </div>
       </CardContent>
